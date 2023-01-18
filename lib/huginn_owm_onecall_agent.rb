b5a46b2a23ba7520311c293da315719aa8678380
Viperinius <viperinius@gmx.de>
2023-01-18 19:44:52 +0100

    register stringifier agent

--- lib/huginn_owm_onecall_agent.rb
+++ lib/huginn_owm_onecall_agent.rb
@@ -2,3 +2,4 @@ require 'huginn_agent'
 
 #HuginnAgent.load 'huginn_owm_onecall_agent/concerns/my_agent_concern'
 HuginnAgent.register 'huginn_owm_onecall_agent/owm_onecall_agent'
+HuginnAgent.register 'huginn_owm_onecall_agent/owm_event_stringifier_agent'
